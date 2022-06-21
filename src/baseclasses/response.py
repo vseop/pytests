1ff4b014002da8cf842fa2a31a9584497cf2ed6e
vseop <ivpython@mail.ru>
2022-06-21 21:46:09 +0300

    updated: src.baseclasses.response.py, test_something.py, configurations.py created: enums.user_enums.py, schemas.user.py

--- src/baseclasses/response.py
+++ src/baseclasses/response.py
@@ -1,28 +1,29 @@
-from jsonschema import validate
-
 from src.enums.global_enums import GlobalErrorMessages
 
 
 class Response:
     def __init__(self, response):
         self.response = response
-        self.response_json = response.json()
+        self.response_json = response.json().get('data')
         self.response_status = response.status_code
 
     def validate(self, schema):
         if isinstance(self.response_json, list):
             for item in self.response_json:
                 schema.parse_obj(item)
-                # validate(item, schema)
         else:
             schema.parse_obj(self.response_json)
-            # validate(self.response_json, schema)
         return self
 
     def assert_status_code(self, status_code):
         if isinstance(status_code, list):
-            assert self.response_status in status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+            assert self.response_status in status_code, self
         else:
-            assert self.response_status == status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
-
+            assert self.response_status == status_code, self
         return self
+
+    def __str__(self):
+        return \
+            f"\nStatus code: {self.response_status} \n" \
+            f"Requested url: {self.response.url} \n" \
+            f"Response body: {self.response_json}"
